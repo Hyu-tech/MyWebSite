06c9f746e13459442f6999dc82a3caada1b3914b
hyu <hyutechnical@gmail.com>
2021-12-30 11:03:46 +0900

    index and,introduce

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8" name="discription" content="Hyuの実験場というWebページです">
+  <title>Hyuの実験場</title>
+  <!-- <link rel="icon" href="favicon.ico"> -->
+</head>
+
+<body>
+
+  <header>
+    <h1>このWebサイトについて</h1>
+    <p>このWebサイトはHyuのポートフォリオ兼自由に
+      Web技術を試す実験場です。
+      <br>
+      やってみたいことや技術力を上げるために色々やっていきます。
+    </p>
+  </header>
+
+  <article>
+    <h1>現在の状況について</h1>
+    <p><time>2021/9</time>現在dotinstallを利用してHTMLを勉強しています。<br>
+      CSS、javascriptなどのフロントサイド言語だけでなくPHP,Pythonなんかのバックエンド言語も学ぶ予定です。
+      
+    </p>
+  </article>
+</body>
